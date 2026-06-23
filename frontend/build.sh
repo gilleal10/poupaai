5a59d4e303b5364a1a243fab9fe1e9f63d05b67b
gilleal10 <leal.gilleal10@gmail.com>
2026-06-22 22:31:02 -0400

    Update build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,6 +2,8 @@
 echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
+echo "Creating web configuration..."
+flutter create . --platforms web
 echo "Building Flutter Web..."
 flutter pub get
 flutter build web
